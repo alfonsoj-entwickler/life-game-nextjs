5932edc8855c033e26e4982e8bfcc1ebd3ca3c28
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-09-22 19:27:23 +0200

    [TASK] create a basic header and footer

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return(
+        <footer className="fixed bottom-0 w-screen h-16 bg-yellow-100 opacity-50 z-10"></footer>
+    )
+}
+
+export default Footer;
---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return(
+        <header className="fixed top-0 w-screen h-16 bg-red-100 opacity-50 z-10"></header>
+    )
+}
+
+export default Header;
