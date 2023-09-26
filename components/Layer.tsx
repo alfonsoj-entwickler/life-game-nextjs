b802d5c8a9d079801ed19391c2e2bcaf0c767fb1
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-09-26 22:00:52 +0200

    [TASK] update layer

--- components/Layer.tsx
+++ components/Layer.tsx
@@ -1,64 +1,38 @@
 "use client";
 import { useState, useEffect } from "react";
-import { MouseEvent } from "react";
 import Cell from "./Cell";
 import { Cell as CellType } from "@/types/Cell";
 import useWindowSize from "@/hooks/useWindowSize";
+import randomModelCell from "@/helpers/randomModelCell";
 
 const Layer = () => {
   const [cells, setCells] = useState<CellType[]>([]);
-  const windowSize = useWindowSize()
+  const windowSize = useWindowSize();
 
-  useEffect(()=>{
-    if(windowSize.width && windowSize.height){
-      const sizeArray = Math.floor(windowSize.width/50) * Math.floor(windowSize.height/50);
+  useEffect(() => {
+    if (windowSize.width && windowSize.height) {
+      const sizeArray =
+        Math.floor(windowSize.width / 50) * Math.floor(windowSize.height / 50);
 
-      const cellsArray = Array.from(Array(sizeArray)).fill({
-        id: "1",
-        action: true,
-        model: "1",
-        active: false, 
-      },).map((cell,index) => ({ ...cell, id: index}))
- 
-      
+      const cellsArray = Array.from(Array(sizeArray))
+        .fill({
+          active: false,
+        })
+        .map((cell, index) => ({
+          ...cell,
+          id: index,
+          model: `${randomModelCell(5)}`,
+          rotate: `${randomModelCell(5)}`,
+        }));
+
       setCells(cellsArray);
     }
-    
   }, [windowSize]);
 
-  const handleMove = (e: MouseEvent) => {
-    //console.log("MouseEvent--> ", e);
-  };
-
-  const handleClick = (e: MouseEvent) => {
-    //console.log("ClickEvent--> ",e, e.pageX, e.pageY);
-    /*
-      setCells([
-        ...cells,
-        {
-          id: "1",
-          action: true,
-          model: "1",
-          active: true, 
-        },
-      ])
-    */
-    ;
-  };
-
   return (
-    <div
-      //onMouseMove={(e) => handleMove(e)}
-      onClick={(e) => handleClick(e)}
-      className="relative w-full h-screen flex flex-wrap justify-center items-center"
-    >
+    <div className="relative w-full h-screen flex flex-wrap justify-center items-center">
       {cells.length > 0 &&
-        cells.map((item) => (
-          <Cell
-            key={`cell-${item.id}`}
-            item={item}
-          />
-        ))}
+        cells.map((item) => <Cell key={`cell-${item.id}`} item={item} />)}
     </div>
   );
 };
