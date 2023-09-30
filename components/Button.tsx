704436130b1216bbe99e081583bb6349f7261006
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-09-30 09:13:57 +0200

    [TASK] update component button

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,14 +2,14 @@ import { ReactNode } from "react";
 
 type Props = {
   text: string;
-  border: boolean;
+  border?: boolean;
   call?: () => void;
   children?: ReactNode;
 };
 
 const Button = ({ text, border = false, call, children }: Props) => {
   return (
-    <button type="button" aria-label={text} className="">
+    <button type="button" aria-label={text} className="flex justify-center items-center space-x-2 text-sm font-medium text-gray-900">
       {children}
       <span>{text}</span>
     </button>
