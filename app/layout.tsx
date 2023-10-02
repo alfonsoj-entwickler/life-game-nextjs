9b804b3426259fecf1addf8a2de3d5991e6c8dda
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-10-02 18:36:47 +0200

    [TASK] change font to bangers font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Bangers } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const bangers = Bangers({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={bangers.className}>{children}</body>
     </html>
   )
 }
