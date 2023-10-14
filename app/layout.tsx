7189524514af347084bb6e582c754da3b5b7b9ac
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-10-14 12:39:37 +0200

    [TASK] adapt all main component in window

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,30 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Bangers } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Bangers } from "next/font/google";
 
 const bangers = Bangers({
-  weight: '400',
-  subsets: ['latin'],
-  display: 'swap',
-})
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
-  title: 'Life Game',
-  description: 'Playing with cells',
-}
+  title: "Life Game",
+  description: "Playing with cells",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={bangers.className}>{children}</body>
+      <body
+        className={`${bangers.className} relative flex h-screen flex-col items-center justify-between bg-animate overflow-hidden`}
+      >
+        {children}
+      </body>
     </html>
-  )
+  );
 }
