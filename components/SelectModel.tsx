49b4998813ee5052696dc658fa76c4de67cf7490
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-10-25 19:50:36 +0200

    [TASK] add interactive select with cells models

--- components/SelectModel.tsx
+++ components/SelectModel.tsx
@@ -1,19 +1,33 @@
-const SelectModel = () => {
-    return(
-        <div className="flex space-x-2 text-lg">
-          <label htmlFor="model-cell">Choose a model:</label>
-          <select name="models" id="model-cell" className="outline-none">
-            <option value="random">Random</option>
-            <option value="cell-1">Cell 1</option>
-            <option value="cell-2">Cell 2</option>
-            <option value="cell-3">Cell 3</option>
-            <option value="cell-4">Cell 4</option>
-            <option value="cell-5">Cell 5</option>
-            <option value="cell-6">Cell 6</option>
-            <option value="cell-7">Cell 7</option>
-          </select>
-        </div>
-    )
-}
+import { ChangeEvent } from "react";
+import { useActionConfig } from "@/context/useActionConfig";
 
-export default SelectModel
+const SelectModel = () => {
+  const { modelCell, setModelCells } = useActionConfig();
+
+  const handlerSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    setModelCells(e.target.value);
+  };
+  return (
+    <div className="flex space-x-2 text-lg">
+      <label htmlFor="model-cell">Choose a model:</label>
+      <select
+        name="models"
+        id="model-cell"
+        className="outline-none"
+        value={modelCell}
+        onChange={(e) => handlerSelect(e)}
+      >
+        <option value="random">Random</option>
+        <option value="1">Cell 1</option>
+        <option value="2">Cell 2</option>
+        <option value="3">Cell 3</option>
+        <option value="4">Cell 4</option>
+        <option value="5">Cell 5</option>
+        <option value="6">Cell 6</option>
+        <option value="7">Cell 7</option>
+      </select>
+    </div>
+  );
+};
+
+export default SelectModel;
