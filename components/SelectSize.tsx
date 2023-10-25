4c8091be39b45c7b34f3ce90611a130166cd1ae4
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-10-25 19:50:26 +0200

    [TASK] add interactive select with cells size

--- components/SelectSize.tsx
+++ components/SelectSize.tsx
@@ -1,15 +1,35 @@
-const SelectSize = () => {
-    return(
-        <div className="flex space-x-2 text-lg outline-none">
-          <label htmlFor="size-cell">Choose a size:</label>
-          <select name="size" id="size-cell" className="outline-none">
-            <option value="60">60</option>
-            <option value="50">50</option>
-            <option value="40">40</option>
-            <option value="30">30</option>
-          </select>
-        </div>
-    )
-}
+import { ChangeEvent } from "react";
+import { useActionConfig } from "@/context/useActionConfig";
+import useWindowSize from "@/hooks/useWindowSize";
 
-export default SelectSize
+const SelectSize = () => {
+  const { sizeCell, setSizeCells, resetCells } = useActionConfig();
+  const windowSize = useWindowSize();
+
+  const handlerSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSizeCells(Number(e.target.value));
+    //ToDo: delete reset layer and update cells
+    resetCells(windowSize.width, windowSize.height);
+  };
+  return (
+    <div className="flex space-x-2 text-lg outline-none">
+      <label htmlFor="size-cell">Choose a size:</label>
+      <select
+        name="size"
+        id="size-cell"
+        className="outline-none"
+        value={sizeCell}
+        onChange={(e) => handlerSelect(e)}
+      >
+        <option value="70">70</option>
+        <option value="60">60</option>
+        <option value="50">50</option>
+        <option value="40">40</option>
+        <option value="30">30</option>
+        <option value="20">20</option>
+      </select>
+    </div>
+  );
+};
+
+export default SelectSize;
