8f6e621541519418070eb7b1ca4e1d89eddda071
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-10-27 20:17:45 +0200

    [TASK] create constants to select

---
+++ helpers/valuesSelects.tsx
@@ -0,0 +1,31 @@
+interface SelectSize {
+  value: number;
+  text: string;
+};
+
+export const valuesSize = [
+  {
+    value: 70,
+    text: "70",
+  },
+  {
+    value: 60,
+    text: "60",
+  },
+  {
+    value: 50,
+    text: "50",
+  },
+  {
+    value: 40,
+    text: "40",
+  },
+  {
+    value: 30,
+    text: "30",
+  },
+  {
+    value: 20,
+    text: "20",
+  },
+];
