8abd169a6be6c0e8d9a4ea3bdf53e92406da1f8f
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-11-03 18:52:14 +0100

    [TASK] create a function to get times units

---
+++ helpers/stepTimer.tsx
@@ -0,0 +1,17 @@
+import { TimeType } from "@/types/Time";
+export const stepTimer = (step: TimeType) => {
+  if (step.miliseconds + step.interval > 999) {
+    step.miliseconds = 0;
+    step.seconds = step.seconds + 1;
+  } else {
+    step.miliseconds = step.miliseconds + step.interval;
+  }
+  if (step.seconds > 59) {
+    step.seconds = 0;
+    step.minutes = step.minutes++;
+  }
+  if (step.minutes > 59) {
+    step.minutes = 0;
+    step.hours = step.hours + 1;
+  }
+};
