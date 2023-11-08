57156e2a8154b7c1ce0604c81c02eb33988d079f
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-11-08 18:20:16 +0100

    [TASK] add a function to reset timer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,35 +18,42 @@ const Footer = () => {
     setLayer,
     setWorld,
     setIndex,
+    resetClock,
   } = useLayerConfig();
   const windowSize = useWindowSize();
 
   return (
     <footer className="w-screen h-[4rem] text-gray-900 bg-[#23d5ab] z-10">
       <div className="h-full flex items-center space-x-6 px-10">
-        <div className={`flex w-2/5 space-x-6 ${stateWorld && "pointer-events-none"}`}>
-        
-              <SelectSize />
-              <SelectModel />
-              <Button
-                text="Reset"
-                onClick={(e) => resetCells(windowSize.width, windowSize.height)}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
-                  />
-                </svg>
-              </Button>
+        <div
+          className={`flex w-2/5 space-x-6 ${
+            stateWorld && "pointer-events-none"
+          }`}
+        >
+          <SelectSize />
+          <SelectModel />
+          <Button
+            text="Reset"
+            onClick={(e) => {
+              resetCells(windowSize.width, windowSize.height);
+              resetClock();
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+              />
+            </svg>
+          </Button>
         </div>
         <div className="flex w-1/5 justify-center">
           {!stateWorld ? (
@@ -93,7 +100,11 @@ const Footer = () => {
             </Button>
           )}
         </div>
-        <div className={`flex w-2/5 justify-end space-x-6 ${stateWorld && "pointer-events-none"}`}>
+        <div
+          className={`flex w-2/5 justify-end space-x-6 ${
+            stateWorld && "pointer-events-none"
+          }`}
+        >
           <Switch
             onText="On Map"
             offText="Off Map"
