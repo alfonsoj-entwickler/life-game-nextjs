1ba587132d5ac13f855386f647274bf28198708c
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-11-20 20:02:28 +0100

    [CLEANUP] code of switch

--- components/Switch.tsx
+++ components/Switch.tsx
@@ -5,12 +5,7 @@ type Props = {
   handlerChange: (status: boolean) => void;
 };
 
-const Switch = ({
-  onText,
-  offText,
-  statusChecked,
-  handlerChange,
-}: Props) => {
+const Switch = ({ onText, offText, statusChecked, handlerChange }: Props) => {
   return (
     <div className="inline-flex items-center">
       <label className="relative cursor-pointer">
@@ -25,7 +20,9 @@ const Switch = ({
           className={`w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-red-300 dark:peer-focus:ring-red-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-red-600`}
         ></div>
       </label>
-      <span className="ml-3 text-lg font-medium">{statusChecked ? onText: offText}</span>
+      <span className="ml-3 text-lg font-medium">
+        {statusChecked ? onText : offText}
+      </span>
     </div>
   );
 };
