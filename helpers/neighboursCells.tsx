93ec169a285009306ff5367b3757c87d91efe1e4
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-11-23 18:51:38 +0100

    [TASK] optimize operations

--- helpers/neighboursCells.tsx
+++ helpers/neighboursCells.tsx
@@ -5,12 +5,17 @@ export const getNeighboursCells = (
   x: number,
   y: number
 ): number[] => {
-  let i_topleft = (x - 1) * cols - (cols - 1 - y);
-  let i_bottomleft = (x + 1) * cols - (cols - 1 - y);
-  let i_top = (x - 1) * cols - (cols - y);
-  let i_bottom = (x + 1) * cols - (cols - y);
-  let i_topright = (x - 1) * cols - (cols + 1 - y);
-  let i_bottomright = (x + 1) * cols - (cols + 1 - y);
+  const x_top = (x - 1) * cols,
+    x_bottom = (x + 1) * cols;
+  const y_left = (cols - 1) - y,
+    y_right = (cols + 1) - y;
+  const horizontal = cols - y;
+  let i_topleft = x_top - y_left;
+  let i_top = x_top - horizontal;
+  let i_topright = x_top - y_right;
+  let i_bottomleft = x_bottom - y_left;
+  let i_bottom = x_bottom - horizontal;
+  let i_bottomright = x_bottom - y_right;
   let i_left = i + 1;
   let i_right = i - 1;
   return [
