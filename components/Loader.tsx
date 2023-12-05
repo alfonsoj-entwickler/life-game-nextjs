7e276d20ebe9e3650b8afb46c3ac956c296ca580
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-12-05 19:30:03 +0100

    [TASK] create loader component

---
+++ components/Loader.tsx
@@ -0,0 +1,17 @@
+const Loader = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="sk-chase">
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+      </div>
+      <p className="mt-10 text-4xl">Loading ...</p>
+    </div>
+  );
+};
+
+export default Loader;
