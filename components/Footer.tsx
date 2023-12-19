2062a19187df7fcf1f35ede38b5fa5da2610cb31
alfonsoj-entwickler <77501021+alfonsoj-entwickler@users.noreply.github.com>
2023-12-19 17:20:20 +0100

    [TASK] disable footer in loading state

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ import { useLayerConfig } from "@/context/useLayerConfig";
 import { valuesModels, valuesSize } from "@/helpers/valuesSelects";
 
 const Footer = () => {
-  const { sizeCell, setSizeCells, modelCell, setModelCells, resetCells } = useActionConfig();
+  const { sizeCell, setSizeCells, modelCell, setModelCells, resetCells, loading } = useActionConfig();
   const {
     stateAnimations,
     stateLayer,
@@ -42,7 +42,7 @@ const Footer = () => {
       <div className="h-full flex items-center space-x-6 px-10">
         <div
           className={`flex items-center w-2/5 space-x-6 ${
-            stateWorld && "pointer-events-none"
+            (stateWorld || loading) && "pointer-events-none"
           }`}
         >
           <Select
@@ -86,53 +86,59 @@ const Footer = () => {
           </Button>
         </div>
         <div className="flex w-1/5 justify-center">
-          {!stateWorld ? (
-            <Button
-              text="Play"
-              onClick={(e) => {
-                setWorld(true);
-                setLayer(true);
-              }}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-6 h-6"
+          <div
+            className={`flex w-2/5 justify-end space-x-6 ${
+              loading && "pointer-events-none"
+            }`}
+          >
+            {!stateWorld ? (
+              <Button
+                text="Play"
+                onClick={(e) => {
+                  setWorld(true);
+                  setLayer(true);
+                }}
               >
-                <path
-                  fillRule="evenodd"
-                  d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </Button>
-          ) : (
-            <Button
-              text="Pause"
-              onClick={(e) => {
-                setWorld(false);
-                setLayer(false);
-              }}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-6 h-6"
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm14.024-.983a1.125 1.125 0 010 1.966l-5.603 3.113A1.125 1.125 0 019 15.113V8.887c0-.857.921-1.4 1.671-.983l5.603 3.113z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </Button>
+            ) : (
+              <Button
+                text="Pause"
+                onClick={(e) => {
+                  setWorld(false);
+                  setLayer(false);
+                }}
               >
-                <path
-                  fillRule="evenodd"
-                  d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM9 8.25a.75.75 0 00-.75.75v6c0 .414.336.75.75.75h.75a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75H9zm5.25 0a.75.75 0 00-.75.75v6c0 .414.336.75.75.75H15a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75h-.75z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </Button>
-          )}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM9 8.25a.75.75 0 00-.75.75v6c0 .414.336.75.75.75h.75a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75H9zm5.25 0a.75.75 0 00-.75.75v6c0 .414.336.75.75.75H15a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75h-.75z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </Button>
+            )}
+            </div>
         </div>
         <div
           className={`flex w-2/5 justify-end space-x-6 ${
-            stateWorld && "pointer-events-none"
+            (stateWorld || loading) && "pointer-events-none"
           }`}
         >
           <Switch
